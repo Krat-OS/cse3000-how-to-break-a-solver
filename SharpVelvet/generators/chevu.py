e4dc58c22d87265cbef2558b3cf4f56bb154a88a
Chevuu <vukjurisic@gmail.com>
2025-01-12 21:18:39 +0100

    250 instances

--- SharpVelvet/generators/chevu.py
+++ SharpVelvet/generators/chevu.py
@@ -357,14 +357,14 @@ def main():
     parser.add_argument("--threads", type=int, default=4,
                         help="Number of threads to use for parallel generation (default: 4).")
 
-    parser.add_argument("--min-clauses", type=int, default=500,
-                        help="Minimum number of clauses (default=500).")
-    parser.add_argument("--max-clauses", type=int, default=500,
-                        help="Maximum number of clauses (default=500).")
-    parser.add_argument("--min-vars", type=int, default=200,
-                        help="Minimum number of variables (default=200).")
-    parser.add_argument("--max-vars", type=int, default=200,
-                        help="Maximum number of variables (default=200).")
+    parser.add_argument("--min-clauses", type=int, default=250,
+                        help="Minimum number of clauses (default=250).")
+    parser.add_argument("--max-clauses", type=int, default=250,
+                        help="Maximum number of clauses (default=250).")
+    parser.add_argument("--min-vars", type=int, default=100,
+                        help="Minimum number of variables (default=100).")
+    parser.add_argument("--max-vars", type=int, default=100,
+                        help="Maximum number of variables (default=100).")
     parser.add_argument("--min-clause-len", type=int, default=3,
                         help="Minimum clause length (default=3).")
     parser.add_argument("--max-clause-len", type=int, default=3,
