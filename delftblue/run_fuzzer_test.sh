3f9e0e702b97ceae91f293051276cab88be856a4
Chevuu <vukjurisic@gmail.com>
2025-01-12 18:06:00 +0100

    Set better configs

--- delftblue/run_fuzzer_test.sh
+++ delftblue/run_fuzzer_test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --job-name="sharpvelvet-test-run-5solvers"
 #SBATCH --time=02:00:00
-#SBATCH --ntasks=5                # 5 tasks, one for each solver
-#SBATCH --cpus-per-task=4         # 4 CPUs allocated to each task
-#SBATCH --mem-per-cpu=8G          # 8GB memory per CPU (32GB per task)
-#SBATCH --partition=compute-p2    # Partition to use
+#SBATCH --ntasks=4
+#SBATCH --cpus-per-task=4
+#SBATCH --mem-per-cpu=16G
+#SBATCH --partition=compute-p2
 #SBATCH --account=education-eemcs-courses-cse3000
 
 # Load Miniconda module
